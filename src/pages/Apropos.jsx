10527300891f797782c59588b97cedeffa360ef8
random1531 <loic.raval@gmail.com>
2024-09-02 18:04:09 +0200

    modified:   src/css/logementimg.css 	modified:   src/pages/Apropos.jsx

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -14,8 +14,6 @@ function Apropos() {
                 <Details summary="Respect" >La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</Details>
                 <Details summary="Service" >La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</Details>
                 <Details summary="Sécurité" >La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</Details>
-
-
             </div>
         </>
     )
